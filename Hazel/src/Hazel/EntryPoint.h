333dd261b947984e54b94eb53e2e6fc039c21c3d
Alexander <alex_c007@hotmail.com>
2020-04-23 04:23:41 +0200

    Finalize logging - getting ready for premake!

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,16 +1,24 @@
 #pragma once
 
-#pragma warning(disable : 26812) // disable warning about unscoped enums
-
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	const int major_version = 0;
+	const int minor_version = 1;
+	const int revision = 15;
+
 	Hazel::Log::Init();
-	HZ_CORE_ERROR("Initialized Log!");
-	Hazel::Log::GetClientLogger()->info("Hello!");
+	HZ_CORE_INFO("Initializing Hazel Engine v.{}.{}.{}",
+		major_version, minor_version, revision); // green
+	HZ_INFO("Hello!");                           // green
+
+	HZ_CORE_WARN("some core warn");   // yellow
+	HZ_CORE_TRACE("some core trace"); // red (could it be another color ?)
+	HZ_CORE_FATAL("some core fatal"); // red background, white text
+	HZ_CORE_ERROR("some core error"); // red
 
 	auto app = Hazel::CreateApplication();
 	app->Run();
--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -38,15 +38,16 @@ namespace Hazel
 	};
 }
 
+// Core log macros
+#define HZ_CORE_TRACE(...)   ::Hazel::Log::GetCoreLogger()->trace   (__VA_ARGS__);
+#define HZ_CORE_INFO(...)    ::Hazel::Log::GetCoreLogger()->info    (__VA_ARGS__);
+#define HZ_CORE_WARN(...)    ::Hazel::Log::GetCoreLogger()->warn    (__VA_ARGS__);
+#define HZ_CORE_ERROR(...)   ::Hazel::Log::GetCoreLogger()->error   (__VA_ARGS__);
+#define HZ_CORE_FATAL(...)   ::Hazel::Log::GetCoreLogger()->critical(__VA_ARGS__);
 
-#define HZ_CORE_ERROR(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CORE_WARN(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CORE_INFO(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CORE_TRACE(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CORE_FATAL(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-
-#define HZ_CLIENT_ERROR(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CLIENT_WARN(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CLIENT_INFO(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CLIENT_TRACE(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
-#define HZ_CLIENT_FATAL(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__);
+// Client log macros
+#define HZ_TRACE(...) ::Hazel::Log::GetCoreLogger()->trace   (__VA_ARGS__);
+#define HZ_INFO(...)  ::Hazel::Log::GetCoreLogger()->info    (__VA_ARGS__);
+#define HZ_WARN(...)  ::Hazel::Log::GetCoreLogger()->warn    (__VA_ARGS__);
+#define HZ_ERROR(...) ::Hazel::Log::GetCoreLogger()->error   (__VA_ARGS__);
+#define HZ_FATAL(...) ::Hazel::Log::GetCoreLogger()->critical(__VA_ARGS__);
